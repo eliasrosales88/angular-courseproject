cd575c125ff240f0cf299510a791514f888e9a99
Elias Rosales <eliasrosales88@gmail.com>
2018-05-18 23:43:21 +0200

    agregando ingredientes al recipe-detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,28 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
      private recipes: Recipe[] = [
-        new Recipe('Test Recipe1', 'Test Recipe Desc', 'https://cdn1.medicalnewstoday.com/content/images/articles/269/269143/popular-healthy-foods.jpg'),
-        new Recipe('Test Recipe2', 'Test Recipe Desc', 'https://cdn1.medicalnewstoday.com/content/images/articles/269/269143/popular-healthy-foods.jpg')
-      ];
+        new Recipe(
+            'Pabellon Criollo', 
+            'Plato tipico venezolano (no poner azucar a las caraotas)',
+            'https://cdn1.medicalnewstoday.com/content/images/articles/269/269143/popular-healthy-foods.jpg', 
+            [
+                new Ingredient('Arroz', 1), 
+                new Ingredient('Caraotas',1)
+            ]
+        ),
+        new Recipe(
+            'Asado Negro', 
+            'Carne con un gusto dulcesalado',
+            'https://cdn1.medicalnewstoday.com/content/images/articles/269/269143/popular-healthy-foods.jpg',
+            [
+                new Ingredient('Carne', 1), 
+                new Ingredient('Arroz',1)
+            ]
+        )];
 
     constructor() {
         
